451aad2a44d50ea307f5cb9fe48eee649cde985d
balint <balint@cloudflare.com>
2026-08-05 11:47:37 +0100

    fix: migrate table of contents border usage

--- packages/kumo/src/components/table-of-contents/table-of-contents.tsx
+++ packages/kumo/src/components/table-of-contents/table-of-contents.tsx
@@ -138,7 +138,7 @@ export interface TableOfContentsGroupProps extends Omit<
 }
 
 const NESTED_UL_CLASSES =
-  "flex flex-col gap-2 border-l-2 border-kumo-line [&>li>a]:pl-7 [&>li>button]:pl-7";
+  "flex flex-col gap-2 border-l-2 border-transparent [&>li>a]:pl-7 [&>li>button]:pl-7";
 
 const TableOfContentsGroup = forwardRef<
   HTMLLIElement,
